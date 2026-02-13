19821469e428143fdefe935f73bbf583eb8e68b4
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2026-02-13 05:39:47 -0700

    Added Last used Mods data filepath and property

--- CalradiaForge.Core/Infra/Paths/AppPaths.cs
+++ CalradiaForge.Core/Infra/Paths/AppPaths.cs
@@ -5,7 +5,7 @@
 
 	public static class AppPaths
 		{
-		public static string RootDirectory { get; } = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? AppDomain.CurrentDomain.BaseDirectory;
+		public static string RootDirectory { get; } = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)??AppDomain.CurrentDomain.BaseDirectory;
 
 		private const string ConfigFolderName = "Config";
 		private const string LogsFolderName = "Logs";
@@ -14,15 +14,17 @@
 
 		private const string ModsCurrentFileName = "mods_current.data";
 		private const string ModsBackupFileName = "mods_backup.data";
+		private const string LastUsedModsFileName = "last_used_mods.data";
 		private const string ConfigFileName = "config.json";
 
-		public static string ConfigDirectory => EnsureDirectoryExists(Path.Combine(RootDirectory, ConfigFolderName));
-		public static string LogsDirectory => EnsureDirectoryExists(Path.Combine(RootDirectory, LogsFolderName));
-		public static string ModpacksDirectory => EnsureDirectoryExists(Path.Combine(RootDirectory, ModpacksFolderName));
-		public static string DataDirectory => EnsureDirectoryExists(Path.Combine(RootDirectory, DataFolderName));
-		public static string ModsCurrentFilePath => Path.Combine(DataDirectory, ModsCurrentFileName);
-		public static string ModsBackupFilePath => Path.Combine(DataDirectory, ModsBackupFileName);
-		public static string ConfigFilePath => Path.Combine(ConfigDirectory, ConfigFileName);
+		public static string ConfigDirectory => EnsureDirectoryExists(Path.Combine(RootDirectory,ConfigFolderName));
+		public static string LogsDirectory => EnsureDirectoryExists(Path.Combine(RootDirectory,LogsFolderName));
+		public static string ModpacksDirectory => EnsureDirectoryExists(Path.Combine(RootDirectory,ModpacksFolderName));
+		public static string DataDirectory => EnsureDirectoryExists(Path.Combine(RootDirectory,DataFolderName));
+		public static string ModsCurrentFilePath => Path.Combine(DataDirectory,ModsCurrentFileName);
+		public static string ModsBackupFilePath => Path.Combine(DataDirectory,ModsBackupFileName);
+		public static string LastUsedModsFilePath => Path.Combine(DataDirectory,LastUsedModsFileName);
+		public static string ConfigFilePath => Path.Combine(ConfigDirectory,ConfigFileName);
 
 		private static string EnsureDirectoryExists(string path) {
 			if (!Directory.Exists(path)) {
