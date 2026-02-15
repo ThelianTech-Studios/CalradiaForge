e29dbede7c45ce01c053d4084067b0a5a25e4758
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2026-02-15 00:57:38 -0700

    Created FAQ Page, and added some code to handle opening the repos issues link on github.

---
+++ CalradiaForge.UI/Pages/FaqPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace CalradiaForge.UI.Pages
+	{
+	using System.Diagnostics;
+	using System.Windows;
+	using System.Windows.Controls;
+
+	/// <summary>
+	/// Interaction logic for FaqPage.xaml
+	/// </summary>
+	public partial class FaqPage : Page
+		{
+		public FaqPage() {
+			InitializeComponent();
+			}
+
+		private void OpenGitHubIssues_Click(object sender,RoutedEventArgs e) {
+			Process.Start(new ProcessStartInfo {
+				FileName="https://github.com/ThelianTech-Studios/CalradiaForge/issues",
+				UseShellExecute=true,
+				});
+			}
+		}
+	}
