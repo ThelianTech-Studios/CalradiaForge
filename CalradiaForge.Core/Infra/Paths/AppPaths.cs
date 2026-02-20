5fdb53b237cedd6eadbe3eedb5b1008657e0fed1
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2026-02-19 22:03:54 -0700

    Added the new languages Folderpath for tranlation files.

--- CalradiaForge.Core/Infra/Paths/AppPaths.cs
+++ CalradiaForge.Core/Infra/Paths/AppPaths.cs
@@ -11,8 +11,9 @@
 		private const string LogsFolderName = "Logs";
 		private const string ModpacksFolderName = "Modpacks";
 		private const string DataFolderName = "Data";
+		private const string LanguagesFolderName = "Languages";
 
-		private const string ModsCurrentFileName = "mods_current.data";
+        private const string ModsCurrentFileName = "mods_current.data";
 		private const string ModsBackupFileName = "mods_backup.data";
 		private const string LastUsedModsFileName = "last_used_mods.data";
 		private const string ConfigFileName = "config.json";
@@ -21,7 +22,8 @@
 		public static string LogsDirectory => EnsureDirectoryExists(Path.Combine(RootDirectory,LogsFolderName));
 		public static string ModpacksDirectory => EnsureDirectoryExists(Path.Combine(RootDirectory,ModpacksFolderName));
 		public static string DataDirectory => EnsureDirectoryExists(Path.Combine(RootDirectory,DataFolderName));
-		public static string ModsCurrentFilePath => Path.Combine(DataDirectory,ModsCurrentFileName);
+		public static string LanguagesDirectory => EnsureDirectoryExists(Path.Combine(RootDirectory,LanguagesFolderName));
+        public static string ModsCurrentFilePath => Path.Combine(DataDirectory,ModsCurrentFileName);
 		public static string ModsBackupFilePath => Path.Combine(DataDirectory,ModsBackupFileName);
 		public static string LastUsedModsFilePath => Path.Combine(DataDirectory,LastUsedModsFileName);
 		public static string ConfigFilePath => Path.Combine(ConfigDirectory,ConfigFileName);
