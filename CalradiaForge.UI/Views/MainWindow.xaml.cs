6080a9f5c9ef4056d971d7705a7ea3365b0c90d6
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2026-02-20 00:15:08 -0700

    Removed unreachable code, IF conditional and logic not needed in MouseleftClick event for Titlebar.

--- CalradiaForge.UI/Views/MainWindow.xaml.cs
+++ CalradiaForge.UI/Views/MainWindow.xaml.cs
@@ -78,21 +78,6 @@
 				this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
 				return;
 			}
-			if (this.WindowState == WindowState.Maximized && e.ClickCount == 2) {
-				var mousePos = e.GetPosition(this);
-				double horizonBarRatio = Math.Max(0.0,Math.Min(1.0,mousePos.X / this.ActualWidth));
-
-				var rb = this.RestoreBounds;
-				double targetWidth = rb.Width > 0 ? rb.Width : Math.Max(this.MinWidth,1200);
-				double targetHeight = rb.Height > 0 ? rb.Height : Math.Max(this.MinHeight,700);
-
-				var screenPoint = this.PointToScreen(mousePos);
-
-				this.WindowState = WindowState.Normal;
-				this.Left = screenPoint.X - (horizonBarRatio * targetWidth);
-				this.Top = screenPoint.Y - mousePos.Y;
-				return;
-			}
 			try {
 				this.DragMove();
 			} catch (Exception ex) {
