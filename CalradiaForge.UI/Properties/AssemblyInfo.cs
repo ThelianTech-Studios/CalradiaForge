aa6dd7bf92b94c51b6c237da8da89f27866fc16b
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2026-02-20 06:26:56 -0700

    updating project build params for beta release.

---
+++ CalradiaForge.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Windows;
+using System.Windows.Resources;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: ObfuscateAssembly(true)]
