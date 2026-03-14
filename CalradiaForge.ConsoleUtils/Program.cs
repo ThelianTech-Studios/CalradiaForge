9b5968d9929fbb4bd1da55052fdd074d7f734212
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2026-03-14 17:11:42 -0600

    Added ConsoleUtils Project, current only is one tool. generates a new en-US.json file for using when I add or remove Translation Keys from the UI and Core. taht way im not manually editing en-us.json files ever again and its automized partially.

---
+++ CalradiaForge.ConsoleUtils/Program.cs
@@ -0,0 +1,13 @@
+﻿using CalradiaForge.Core.Infra.Localization;
+using CalradiaForge.Core.Infra.Paths;
+
+var filepath = AppPaths.DefaultLanguageFilePath;
+var manager = new TranslationManager(
+	AppPaths.LanguagesDirectory,
+	AppPaths.LanguagesManifestFilePath,
+	AppPaths.DefaultLanguageFilePath);
+
+manager.DefaultEnglishLanguageFile(TranslationStrings.GetDefaultTranslations(), filepath);
+
+Console.WriteLine($"Default English language file created at: {filepath}");
+Console.ReadKey();
