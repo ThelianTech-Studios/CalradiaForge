dd09af34c06053f0a9a38d3879998b543180a6fe
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2026-06-12 14:12:16 -0600

    Updated this Utility to have a selection menu for running different utilities for CalradiaForge Development

--- CalradiaForge.ConsoleUtils/Program.cs
+++ CalradiaForge.ConsoleUtils/Program.cs
@@ -1,13 +1,37 @@
 ﻿using CalradiaForge.Core.Infra.Localization;
 using CalradiaForge.Core.Infra.Paths;
 
-var filepath = AppPaths.DefaultLanguageFilePath;
-var manager = new TranslationManager(
-	AppPaths.LanguagesDirectory,
-	AppPaths.LanguagesManifestFilePath,
-	AppPaths.DefaultLanguageFilePath);
+internal class Program {
+	private static void Main(string[] args) {
+		bool check = true;
+		do {
+			Console.WriteLine("Select a Program to run: \n 1. Create Default Language File \n 2. Exit \n (Use the number keys to select)");
+			var choice = Console.ReadKey().KeyChar;
+			switch (choice) {
+				case '1':
+					CreateDefaultLanguageFile();
+					break;
+				case '2':
+					check = false;
+					break;
+				default:
+					Console.WriteLine("\nInvalid choice. Please select a valid option.");
+					break;
+			}
+		} while (check);
 
-manager.DefaultEnglishLanguageFile(TranslationStrings.GetDefaultTranslations(), filepath);
+		static void CreateDefaultLanguageFile() {
+			var filepath = AppPaths.DefaultLanguageFilePath;
+			var manager = new TranslationManager(
+				AppPaths.LanguagesDirectory,
+				AppPaths.LanguagesManifestFilePath,
+				AppPaths.DefaultLanguageFilePath);
 
-Console.WriteLine($"Default English language file created at: {filepath}");
-Console.ReadKey();
+			manager.DefaultEnglishLanguageFile(TranslationStrings.GetDefaultTranslations(), filepath);
+
+			Console.WriteLine($"Default English language file created at: {filepath}");
+			Console.ReadKey();
+			Console.Clear();
+		}
+	}
+}
