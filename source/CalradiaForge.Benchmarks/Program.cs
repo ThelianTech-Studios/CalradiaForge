580f3a4e19e383fd701a86b75953a39b69832aad
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2026-07-15 10:26:38 -0600

    Added Benchmark project to solution for benchmark stress testing of the app.

---
+++ source/CalradiaForge.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// -----------------------------------------------------------------------
+
+namespace CalradiaForge.Benchmarks;
+
+using BenchmarkDotNet.Running;
+
+internal class Program {
+	static void Main(string[] args) {
+		var _ = BenchmarkRunner.Run(typeof(Program).Assembly);
+	}
+}
